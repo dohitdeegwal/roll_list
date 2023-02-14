75f7022dbf055263fa4372e507a4d038f375da0a
dohitdeegwal <76277872+dohitdeegwal@users.noreply.github.com>
2023-02-14 18:42:45 +0530

    corrected DB connection

--- index.js
+++ index.js
@@ -5,9 +5,11 @@ const morgan = require('morgan');
 const dbUser = process.env.atlasUser;
 const dbPass = process.env.atlasPassword;
 const clusterName = process.env.atlasClusterName;
-const dbName = process.env.atlasDBName;
+const dbName = 'StudentDetails';
 const dbURI = `mongodb+srv://${dbUser}:${dbPass}@${clusterName}/${dbName}?retryWrites=true&w=majority`;
 const PORT = 3000;
+
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 var Schema = mongoose.Schema;
 
@@ -30,6 +32,7 @@ app.get('/', function(req, res) {
 });
 
 app.get('/student', function(req, res) {
+
     // filter data by query params (if any) through regex for name and dept, and exact match for roll and year
     var filter = {};
     if (req.query.roll) {
@@ -54,15 +57,17 @@ app.get('/student', function(req, res) {
     // find data from db, also handle if no data found
     Data.find(filter, function(err, data) {
         if (err) {
-            res.status(500).send('Internal Server Error');
-        } else {
-            if (data.length) {
-                res.json(data);
-            } else {
-                res.status(404).send('No Data Found');
-            }
+            res.status(500).send('Error in DB');
+            return;
         }
+        if (data.length === 0) {
+            res.status(404).send('No Data Found');
+            return;
+        }
+        res.send(data);
     });
+
+
 });
 
 app.listen(PORT, function() {
